f6abb51db239af0d1209ef4c98dcf79b5765259f
White Hat <130337052+White1Hats@users.noreply.github.com>
2023-10-31 21:54:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-
-const options = {method: 'POST', headers: {Authorization: '1083600201878605825-8bfb3dc71c1f584fe9e95ce362336dc7f73eddedaec91b87cc4c6072307ac143'}};
-
-fetch('https://api.squarecloud.app/v2/apps/6bd2ff0c5b184bcfbd421c00a4288470/start', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
-
-
 const Discord = require("discord.js")
 
 const config = require("./config.json")
